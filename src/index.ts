5db262f8464fb9b6fdbb2c207c0cbc65be7f831f
Gabriel Araújo <gabriel@kenzie.com.br>
2022-05-02 10:38:26 -0300

    contato inicial com ts

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,77 @@ let user = {
 console.log(user.trabalho.empresa);
 
 // Erro na tipagem - reatribuição
-float = "asdasduh";
+// float = "asdasduh";
 
 // Erro na tipagem - any type
-const logUser = (user) => {};
+// const logUser = (user) => {};
+
+const logName = (name: string) => {
+  console.log(name);
+};
+
+interface UserData {
+  name: string;
+  email: string;
+  idade: number;
+  trabalho: WorkData;
+}
+
+interface WorkData {
+  empresa: string;
+  quantidade_funcionarios?: number;
+}
+
+const logUser = ({ email, idade, name, trabalho }: UserData) => {
+  console.log(email, idade);
+};
+
+logUser({
+  email: "gabriel@kenzie.com.br",
+  idade: 20,
+  name: "Gabriel",
+  trabalho: {
+    empresa: "Kenzie",
+  },
+});
+
+// Tipando um objeto com a ferramenta type
+type Car = {
+  model: string;
+};
+
+// Extendendo types //
+type ExtendedCar = Car & {
+  color: string;
+};
+
+// Extenndedo uma interface
+interface ExtendedUserData extends UserData {
+  familyName: string | number;
+}
+
+const userArray: UserData[] = [
+  {
+    email: "gabriel@kenzie.com.br",
+    idade: 20,
+    name: "Gabriel",
+    trabalho: {
+      empresa: "Kenzie",
+    },
+  },
+];
+
+const listOneUser = (
+  users: UserData[],
+  email: string
+): UserData | undefined => {
+  const user = users.find((user) => user.email === email);
+
+  if (!user) {
+    return;
+  }
+
+  return user;
+};
+
+const findUser = listOneUser(userArray, "gabriel@kenzie.com.br");
